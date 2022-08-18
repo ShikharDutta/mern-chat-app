63c62e0c0a9772e7548d46c1326d01f0be08446b
Shikhar <shikhardutta.19je0769@ece.iitism.ac.in>
2022-08-19 01:51:39 +0530

    firstCommit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+//import { Button } from '@chakra-ui/react'
+import React from "react";
+import { Route, Routes} from "react-router-dom";
+import Homepage from './Pages/Homepage';
+import ChatPage from './Pages/ChatPage';
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+      <Route exact path="/" element={<Homepage/>} />
+      <Route path="/chats" element={<ChatPage/>} />
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/ChatPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Box} from "@chakra-ui/react";
+//import axios from 'axios';
+import { ChatState } from '../Context/ChatProvider';
+import SideDrawer from '../components/miscellaneous/SideDrawer';
+import MyChats from '../components/MyChats';
+import ChatBox from '../components/ChatBox';
+import { useState } from 'react';
+
+const ChatPage = () => {
+    const {user}=ChatState();
+    const [fetchAgain, setFetchAgain] = useState();
+    return (<div style={{width:'100%'}}>
+      {user &&<SideDrawer/>}
+      <Box
+        display ='flex'
+        justifyContent='space-between'
+        w ='100%'
+        h='91.5vh'
+        p='10px'
+      >
+        {user&&<MyChats fetchAgain={fetchAgain}/>}
+        {user&&<ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
+      </Box>
+    </div>
+)}
+
+export default ChatPage;
+// import React from 'react'
+
+// const ChatPage = () => {
+//   return (
+//     <div>ChatPage</div>
+//   )
+// }
+
+// export default ChatPage
